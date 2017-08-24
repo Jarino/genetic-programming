91a496301bf1f2907f61f286fbe06932cf5d9cdc
Jarino <jaroslavloebl@gmail.com>
2017-08-24 11:14:18 +0200

    remove unused files

--- tree/node.py
+++
@@ -1,9 +0,0 @@
-
-class Node():
-    def __init__(self, id, value=None, children=None):
-        self.id = id
-        self.value = value
-        self.children = children
-
-    def has_children(self):
-        return self.children is not None or len(self.children) != 0
